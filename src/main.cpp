f3ae18dec3d0ee3f8126e8ebe9d3d42c1b5e403a
neilpf2014 <neilpf2014@gmail.com>
2022-06-05 02:51:09 -0400

    C4

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,36 @@
 #include <PushButton.h>
 
 /*
-** To do Add code to read 8 buttons
+** Robot Combat Arena Control Code
+** Using Ardunio framework / ESP32 (maybe STM32 with GPIO changes)
+** Read 8 buttons
 ** ready and tap out for 2 teams
 ** Match start / pause / end /reset for ref
 ** Serial out to hacked iCruze display boards
+** Will control 1 or 2 "stoplight" towers with signal horns
 ** NF 2022/06/04
 
 */
 
-// button GPIO's
+// State diagram
+/* no match states:
+    Reset and ready for start ( solid yellow )
+      team a ready
+      team b ready
+      both teams ready (blinking yellow)
+    match pause (blinking yellow or blinking red / match timer paused)
+    match tapout (blinking red)
+    match end by KO (blinking red)
+    match end by time (solid red)
+  Match states
+    Start pressed (3 secs of blinking yellow followed by green.  Match timer starts then)
+    start pressed after pause (same as above with timer resuming)
+    active match (solid green)
+    10 sec count down (blinking green)
+  Horn will sound at match end, time up or team tapout
+*/
+
+// button GPIO's ESP32 / Change for STM32
 #define TEAM_A_START 23
 #define TEAM_A_END 22
 #define TEAM_B_START 33
@@ -26,8 +47,10 @@
 #define HORN 13
 
 #define MATCH_LEN 180 // match len in sec ( 3 min)
+#define MATCH_END_WARN 15 // ending warn time sec
 #define BLINK_DELAY 500 // in ms
-#define MAIN_LOOP_DELAY 3 // in ms
+#define STARTUP_DELAY 3000 //3 sec
+#define MAIN_LOOP_DELAY 5 // in ms
 
 PushButton Start_A(TEAM_A_START);
 PushButton End_A(TEAM_A_END);
@@ -47,11 +70,13 @@ enum MatchState{ all_ready, team_a_ready, team_b_ready, starting, in_progress, e
 MatchState g_match;
 uint8_t g_Match_Reset;
 
-bool GameOn;  // do we need this ?
-
 uint64_t gMatchRunTime; //match run time ms
 uint64_t gMatchStartTime; //match start time ms
+uint64_t MatchSecRemain; // sec remaining
 uint8_t isTimerRunning;
+u_int64_t gSDtimer; // used for start delay
+u_int64_t gBLtimer; // for light blinking
+uint8_t gBLstate; // for light blinking
 
 // button reading and state setting is done here
 void readBtns(MatchState &match, uint8_t &Match_Reset)
@@ -156,21 +181,79 @@ void readBtns(MatchState &match, uint8_t &Match_Reset)
 
 }
 
-// used to blink the light tower
-void blink(bool &state, uint64_t &lastBlink, u_int8_t ledGPIO)
+// handle lights here
+void setLights(MatchState &match, u_int8_t Match_Reset, u_int64_t &SDtimer)
 {
-  uint8_t rState;
-	if (state > 0)
-	{
-		digitalWrite(ledGPIO, HIGH);
-		rState = 0;
-	}
-	else
-	{
-		digitalWrite(ledGPIO, LOW);
-		rState = 1;
-	}
-	state = rState;
+  
+// all lights off
+  if((Match_Reset) && (match != starting))
+  {
+    digitalWrite(G_LIGHT,LOW);
+    digitalWrite(Y_LIGHT,LOW);
+    digitalWrite(R_LIGHT,LOW);
+  }
+  // match in progress
+  if (match == in_progress)
+  {
+    digitalWrite(G_LIGHT,HIGH);
+    digitalWrite(Y_LIGHT,LOW);
+    digitalWrite(R_LIGHT,LOW);
+  }
+  // match is stopped
+  // if ((g_match != in_progress) || (g_match != starting) || (g_match != paused) || (g_match != unpaused))
+  // not sure if enum can be used this way
+  if ((match < 3) && (match > 6))
+  {
+    digitalWrite(G_LIGHT,LOW);
+    //digitalWrite(Y_LIGHT,LOW);
+    digitalWrite(R_LIGHT,HIGH);
+  }
+  // match is starting
+  // blink yellow for 3 sec, red out, green out
+  if(match == starting)
+  {
+    if((millis()-SDtimer) < STARTUP_DELAY)
+      blink(gBLstate, gBLtimer, Y_LIGHT);
+    else
+      match=in_progress;
+  }
+  // match is paused
+  // red is lit yellow is blinking
+  if(match == paused)
+  {
+    blink(gBLstate, gBLtimer, Y_LIGHT);
+    digitalWrite(R_LIGHT,HIGH);
+  }
+  // match is ending (within 15 sec of end)
+  // blink green
+  if(match == ending)
+  {
+    blink(gBLstate, gBLtimer, G_LIGHT);
+  }
+
+  // to do when everything working
+  // Team A tap blink A tower red
+  // Team B tap blink B tower red
+  // for now just blink both towers red
+}
+
+// used to blink the light tower
+void blink(u_int8_t &state, uint64_t &lastBlink, u_int8_t ledGPIO)
+{
+  if((millis() - lastBlink) > BLINK_DELAY)
+  {
+    if (state > 0)
+    {
+      digitalWrite(ledGPIO, HIGH);
+      state = 0;
+    }
+    else
+    {
+      digitalWrite(ledGPIO, LOW);
+      state = 1;
+    }
+    lastBlink = millis();
+  }
 }
 
 // update and display match time
@@ -184,7 +267,6 @@ void match_timer(u_int64_t StartTime, u_int64_t &timerValue, u_int8_t &running)
 
 void setup() {
   Serial.begin(115200);
-  GameOn = false;
   pinMode(R_LIGHT,OUTPUT);
   pinMode (Y_LIGHT,OUTPUT);
   pinMode (G_LIGHT, OUTPUT);
@@ -197,6 +279,7 @@ void setup() {
   digitalWrite(G_LIGHT,LOW);
   digitalWrite(Y_LIGHT,LOW);
   digitalWrite(R_LIGHT,LOW);
+  // check horn
   digitalWrite(HORN,HIGH);
   delay(200);
   digitalWrite(HORN,LOW);
@@ -206,74 +289,24 @@ void setup() {
   isTimerRunning = false;
   gMatchRunTime = 0;
   gMatchStartTime = 0;
+  gBLtimer = millis();
 }
 
 void loop() 
 {
-  // Button check logic
- /* no match states:
-      Reset and ready for start ( solid yellow )
-        team a ready
-        team b ready
-        both teams ready (blinking yellow)
-      match pause (blinking yellow or blinking red / match timer paused)
-      match tapout (blinking red)
-      match end by KO (blinking red)
-      match end by time (solid red)
-    Match states
-      Start pressed (3 secs of blinking yellow followed by green.  Match timer starts then)
-      start pressed after pause (same as above with timer resuming)
-      active match (solid green)
-      10 sec count down (blinking green)
-    Horn will sound at match end, time up or team tapout
- */
+  
   // Button reading is here
   if ((cmils-BRLmills) > MAIN_LOOP_DELAY)
   {
- 
     readBtns(g_match, g_Match_Reset);
+    // set startup delay - match will be inprogress after this is done
+    if (g_match == starting)
+      gSDtimer = millis();
   }
   // handle lights
   if ((cmils-BRLmills + 2) > MAIN_LOOP_DELAY)
   {
-    // all lights off
-    if((g_Match_Reset) && (g_match != starting))
-    {
-      digitalWrite(G_LIGHT,LOW);
-      digitalWrite(Y_LIGHT,LOW);
-      digitalWrite(R_LIGHT,LOW);
-    }
-    // match in progress
-    if (g_match == in_progress)
-    {
-      digitalWrite(G_LIGHT,HIGH);
-      digitalWrite(Y_LIGHT,LOW);
-      digitalWrite(R_LIGHT,LOW);
-    }
-    // match is stopped
-    // if ((g_match != in_progress) || (g_match != starting) || (g_match != paused) || (g_match != unpaused))
-    // not sure if enum can be used this way
-    if ((g_match < 3) && (g_match > 6))
-    {
-      digitalWrite(G_LIGHT,LOW);
-      //digitalWrite(Y_LIGHT,LOW);
-      digitalWrite(R_LIGHT,HIGH);
-    }
-
-    // match is starting
-    // blink yellow for 3 sec, red out, green out
-
-    // match is paused
-    // red is lit yellow is blinking
-
-    // match is ending (within 15 sec of end)
-    // blink green
-
-    // to do when everything working
-    // Team A tap blink A tower red
-    // Team B tap blink B tower red
-    // for now just blink both towers red
-
+    setLights(g_match, g_Match_Reset, gSDtimer);
   }
   // handle horn
   if ((cmils-BRLmills + 3) > MAIN_LOOP_DELAY)
@@ -293,9 +326,26 @@ void loop()
       isTimerRunning = true;
     }
     //restart after pause
-
+    if((g_match == unpaused)&&(isTimerRunning == false))
+    {
+      isTimerRunning = true;
+      g_match = in_progress;
+    }
     //stop timer
-    
+    if(g_match != in_progress)
+      isTimerRunning = false;
+
+    //update timer
+    if(((g_match == in_progress)||(g_match == ending))&&(isTimerRunning == true))
+    {
+      match_timer(gMatchStartTime, gMatchRunTime, isTimerRunning);
+      if(isTimerRunning == false)
+        g_match = time_up;
+    }
+    // check for match ending
+    MatchSecRemain = MATCH_LEN - ((gMatchRunTime - gMatchStartTime)/1000);
+    if (MatchSecRemain < MATCH_END_WARN)
+      g_match  = ending;
   }
 
   // update char display

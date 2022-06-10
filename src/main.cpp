8afde0eeb894d6f68795bac62a57c98b2802c757
neilpf2014 <neilpf2014@gmail.com>
2022-06-10 02:56:19 -0400

    C5

--- src/main.cpp
+++ src/main.cpp
@@ -46,11 +46,17 @@
 #define G_LIGHT 5
 #define HORN 13
 
+#define D_SER_TX 17
+#define D_SER_RX 16
+
 #define MATCH_LEN 180 // match len in sec ( 3 min)
 #define MATCH_END_WARN 15 // ending warn time sec
 #define BLINK_DELAY 500 // in ms
 #define STARTUP_DELAY 3000 //3 sec
 #define MAIN_LOOP_DELAY 5 // in ms
+#define HORN_SHORT 1000 //ms
+#define HORN_LONG 2000 //ms
+#define DIS_DELAY 200 //ms display update rate
 
 PushButton Start_A(TEAM_A_START);
 PushButton End_A(TEAM_A_END);
@@ -61,10 +67,12 @@ PushButton GameOver(MATCH_END);
 PushButton GamePause(MATCH_PAUSE);
 PushButton GameReset(MATCH_RESET);
 
-uint64_t pmils;
-uint64_t BRLmills;
-uint64_t cmils;
-uint64_t period = 50;
+uint64_t Btn_timer;
+uint64_t light_timer;
+uint64_t Horn_timer;
+uint64_t Timer_timer;
+uint64_t Display_timer;
+
 // Match state
 enum MatchState{ all_ready, team_a_ready, team_b_ready, starting, in_progress, ending, unpaused, paused, team_a_tap, team_b_tap, time_up, ko_end };
 MatchState g_match;
@@ -74,9 +82,12 @@ uint64_t gMatchRunTime; //match run time ms
 uint64_t gMatchStartTime; //match start time ms
 uint64_t MatchSecRemain; // sec remaining
 uint8_t isTimerRunning;
-u_int64_t gSDtimer; // used for start delay
-u_int64_t gBLtimer; // for light blinking
+uint64_t gSDtimer; // used for start delay
+uint64_t gBLtimer; // for light blinking
 uint8_t gBLstate; // for light blinking
+uint64_t gTootTimer; //for horn
+uint8_t gHornState; // for horn
+uint8_t gHornBlast;
 
 // button reading and state setting is done here
 void readBtns(MatchState &match, uint8_t &Match_Reset)
@@ -181,6 +192,25 @@ void readBtns(MatchState &match, uint8_t &Match_Reset)
 
 }
 
+// used to blink the light tower
+void blink(u_int8_t &state, uint64_t &lastBlink, u_int8_t ledGPIO)
+{
+  if((millis() - lastBlink) > BLINK_DELAY)
+  {
+    if (state > 0)
+    {
+      digitalWrite(ledGPIO, HIGH);
+      state = 0;
+    }
+    else
+    {
+      digitalWrite(ledGPIO, LOW);
+      state = 1;
+    }
+    lastBlink = millis();
+  }
+}
+
 // handle lights here
 void setLights(MatchState &match, u_int8_t Match_Reset, u_int64_t &SDtimer)
 {
@@ -237,25 +267,24 @@ void setLights(MatchState &match, u_int8_t Match_Reset, u_int64_t &SDtimer)
   // for now just blink both towers red
 }
 
-// used to blink the light tower
-void blink(u_int8_t &state, uint64_t &lastBlink, u_int8_t ledGPIO)
+// used to sound horn (yet another timer)
+void soundHorn(u_int8_t &hornBlast, uint64_t &hornTime, u_int32_t tootLen, u_int8_t GPIO)
 {
-  if((millis() - lastBlink) > BLINK_DELAY)
+  u_int8_t state  = hornBlast % 2;
+  if((millis() - hornTime) > tootLen)
   {
     if (state > 0)
     {
-      digitalWrite(ledGPIO, HIGH);
-      state = 0;
+      digitalWrite(GPIO, HIGH);
     }
     else
     {
-      digitalWrite(ledGPIO, LOW);
-      state = 1;
+      digitalWrite(GPIO, LOW);
     }
-    lastBlink = millis();
+    hornTime = millis();
+    hornBlast++;
   }
 }
-
 // update and display match time
 void match_timer(u_int64_t StartTime, u_int64_t &timerValue, u_int8_t &running)
 {
@@ -267,6 +296,8 @@ void match_timer(u_int64_t StartTime, u_int64_t &timerValue, u_int8_t &running)
 
 void setup() {
   Serial.begin(115200);
+  Serial1.setPins(D_SER_RX, D_SER_TX);
+  Serial1.begin(9600);
   pinMode(R_LIGHT,OUTPUT);
   pinMode (Y_LIGHT,OUTPUT);
   pinMode (G_LIGHT, OUTPUT);
@@ -283,8 +314,6 @@ void setup() {
   digitalWrite(HORN,HIGH);
   delay(200);
   digitalWrite(HORN,LOW);
-  cmils = millis();
-  BRLmills = 0;
   g_Match_Reset = false;
   isTimerRunning = false;
   gMatchRunTime = 0;
@@ -296,28 +325,50 @@ void loop()
 {
   
   // Button reading is here
-  if ((cmils-BRLmills) > MAIN_LOOP_DELAY)
+  if ((millis()-Btn_timer) > MAIN_LOOP_DELAY)
   {
     readBtns(g_match, g_Match_Reset);
     // set startup delay - match will be inprogress after this is done
     if (g_match == starting)
       gSDtimer = millis();
+    Btn_timer = millis();
   }
   // handle lights
-  if ((cmils-BRLmills + 2) > MAIN_LOOP_DELAY)
+  if ((millis()-light_timer) > MAIN_LOOP_DELAY + 1)
   {
     setLights(g_match, g_Match_Reset, gSDtimer);
+    light_timer = millis();
   }
   // handle horn
-  if ((cmils-BRLmills + 3) > MAIN_LOOP_DELAY)
+  if ((millis()- Horn_timer) > MAIN_LOOP_DELAY * 2 )
   {
       // short horn on start or pause
-
+      if(g_match ==starting)
+      {
+        while (gHornBlast < 6)
+        {
+          soundHorn(gHornBlast,gTootTimer,HORN_SHORT,HORN);
+        }
+      }
+      else
+        if(g_match < 8)
+          gHornBlast = 0;
       // long horn on match end
+      if(g_match > 7)
+      {
+        while (gHornBlast < 2)
+        {
+          soundHorn(gHornBlast,gTootTimer,HORN_LONG,HORN);
+        }
+      }
+      else
+        if(g_match!=starting)
+          gHornBlast = 0;
+      Horn_timer = millis();
   }
 
   // update match timer
-  if ((cmils-BRLmills + 4) > MAIN_LOOP_DELAY)
+  if ((millis() + Timer_timer) > MAIN_LOOP_DELAY)
   {
     //start timer
     if((g_match == in_progress)&&(isTimerRunning == false))
@@ -346,22 +397,62 @@ void loop()
     MatchSecRemain = MATCH_LEN - ((gMatchRunTime - gMatchStartTime)/1000);
     if (MatchSecRemain < MATCH_END_WARN)
       g_match  = ending;
+    Timer_timer = millis();
   }
 
   // update char display
   // will send info out via serial
-  if ((cmils-BRLmills) > MAIN_LOOP_DELAY)
+  if ((millis()-Display_timer) > DIS_DELAY)
   {
     // using the 2 x 20 line iCruze display
     // refresh display every 200 ms 
-    // display time remaining in min : sec ion first line
+    // display time remaining in min : sec on first line
     // status display on second
-    // --starting count down
-    // --ending countdown
-    // --match paused
-    // --team A tapped
-    // --team B tapped
-    // --KO end
-    // --Time Up end
+    if(g_match == in_progress)
+    {
+      Serial1.println((MatchSecRemain/60) + ':' + (MatchSecRemain % 60) + " Remaining");
+      Serial1.println("Match in Progress");
+    }
+    if(g_match == paused)
+    {
+      Serial1.println((MatchSecRemain/60) + ':' + (MatchSecRemain % 60) + " Remaining");
+      Serial1.println("--Paused --");
+    }
+    if(g_match == starting)
+    {
+      Serial1.println("Starting in: " + (gSDtimer/1000));
+      Serial1.println("Teams Ready-Starting");
+    }
+    if(g_match == time_up)
+    {
+      Serial1.println("--- Time's Up ---");
+      Serial1.println("-- MATCH OVER ---");
+    }
+    if(g_match == team_a_tap)
+    {
+      Serial1.println((MatchSecRemain/60) + ':' + (MatchSecRemain % 60) + " -MATCH OVER-");
+      Serial1.println("--TEAM A TAPOUT--");
+    }
+    if(g_match == team_b_tap)
+    {
+      Serial1.println((MatchSecRemain/60) + ':' + (MatchSecRemain % 60) + " -MATCH OVER-");
+      Serial1.println("--TEAM B TAPOUT--");
+    }
+    if(g_match == ko_end)
+    {
+      Serial1.println((MatchSecRemain/60) + ':' + (MatchSecRemain % 60) + " -MATCH OVER-");
+      Serial1.println("-- BY KNOCKOUT --");
+    }
+    if(g_match == all_ready)
+    {
+      Serial1.println("-- 3 min clock ---");
+      Serial1.println("--- ALL READY ---");
+    }
+    if((g_match != all_ready)&&(g_Match_Reset == true))
+    {
+      Serial1.println("-- WAITING FOR ---");
+      Serial1.println("----- READY -----");
+    }
+    Display_timer = millis();
   }
 }
